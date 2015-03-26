cfd83094b467368424c2179b9e661c1d555eb024
Norman Rusch <norman.rusch@moccu.com>
2015-03-26 11:34:18 +0100

    Write report to path that doesn't exists

--- tests/writer.js
+++ tests/writer.js
@@ -70,6 +70,67 @@ exports['The Writer'] = {
 		});
 	},
 
+	'should save to file in directory that doesn\'t exisits': function(test) {
+		var
+			dir = __dirname + '/test-dir/',
+			path = dir + 'temp-out.xml',
+			writer = new Writer()
+		;
+
+		test.expect(5);
+
+		writer.save(path, function(error, instance) {
+			test.equal(error, null, 'There is an error while writing the file');
+			test.equal(instance, writer, 'The returned instance is not the writer instance');
+
+
+			// Test if exists
+			fs.exists(path, function(exists) {
+				test.ok(exists, 'The file does not exsist');
+
+				// Test if its a file
+				fs.stat(path, function(error, stats) {
+					if (error) {
+						throw error;
+					}
+
+					test.ok(stats.isFile(), 'The path is not a file');
+
+					// Test content of file
+					fs.readFile(path, function(error, data) {
+						if (error) {
+							throw error;
+						}
+
+						test.equal(
+							data,
+							'<?xml version="1.0" encoding="UTF-8"?>\n' +
+							'<testsuites/>',
+							'The output is not the expected one'
+						);
+
+						// Delete file to be done
+						fs.unlink(path, function(error) {
+							if (error) {
+								throw error;
+							}
+
+							// Delete directory to be done
+							fs.rmdir(dir, function(error) {
+								if (error) {
+									throw error;
+								}
+
+								test.done();
+							});
+
+						});
+					});
+				});
+			});
+		});
+	},
+
 	'should save to file that already exists': function(test) {
 		var
 			path = __dirname + '/.temp-out.xml',
